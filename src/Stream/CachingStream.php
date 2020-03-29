960132f48b7ed14ef0a9046f3b16a2dc67f7d968
Riccardo Bastianini <RBastianini@users.noreply.github.com>
2020-03-29 01:33:47 +0100

    Added CachingStream and test.

---
+++ src/Stream/CachingStream.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Polygen\Stream;
+
+use Polygen\Language\Token\Token;
+use Webmozart\Assert\Assert;
+
+/**
+ * A decorator for a TokenStreamInterface object that remembers everything it has read so far and allows rewinding the
+ * stream to any past position.
+ */
+class CachingStream implements TokenStreamInterface
+{
+    /**
+     * @var TokenStreamInterface
+     */
+    private $tokenStream;
+
+    /**
+     * @var Token[]
+     */
+    private $buffer = [];
+
+    private $bufferPointer = 0;
+
+    public function __construct(TokenStreamInterface $stream)
+    {
+        $this->tokenStream = $stream;
+    }
+
+    /**
+     * @return void
+     */
+    public function advance()
+    {
+        if ($this->isEOF()) {
+            throw new \RuntimeException("Trying to read past the end of the stream.");
+        }
+        $this->bufferPointer++;
+        $this->fillTokenToBufferPointer();
+    }
+
+    /**
+     * @return Token
+     */
+    public function nextToken()
+    {
+        $this->fillTokenToBufferPointer();
+        return $this->buffer[$this->bufferPointer];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEOF()
+    {
+        if ($this->buffer === [] && $this->tokenStream->isEOF()) {
+            return true;
+        }
+        return $this->bufferPointer === $this->getBufferSize() - 1 && $this->tokenStream->isEOF();
+    }
+
+    /**
+     * Repositions the stream at the specified offset. 0 is the start of the stream.
+     * @return int
+     */
+    public function tell()
+    {
+        return $this->bufferPointer;
+    }
+
+    /**
+     * @param int $offset
+     * @return void
+     */
+    public function seek($offset)
+    {
+        Assert::greaterThanEq($offset, 0, "Offset can't be negative.");
+        Assert::integer($offset, "Offset must be an integer.");
+        Assert::greaterThanEq($this->bufferPointer, $offset, "Can't seek ahead of the cache.");
+        $this->bufferPointer = $offset;
+    }
+
+    /**
+     * If the buffer pointer is ahead of the buffer, fill the buffer.
+     */
+    private function fillTokenToBufferPointer()
+    {
+        while ($this->bufferPointer >= $bufferSize = $this->getBufferSize()) {
+            $this->buffer[$bufferSize] = $this->tokenStream->nextToken();
+            $this->tokenStream->advance();
+        }
+    }
+
+    private function getBufferSize()
+    {
+        return count($this->buffer);
+    }
+}
---
+++ tests/Integration/Stream/CachingStreamTest.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Tests\Polygen\Integration\Stream;
+
+use Mockery;
+use Polygen\Language\Lexing\Lexer;
+use Polygen\Language\Token\Token;
+use Polygen\Stream\CachingStream;
+use Polygen\Stream\TokenStream;
+use Polygen\Stream\TokenStreamInterface;
+
+class CachingStreamTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_knows_when_it_is_at_the_end_of_the_stream_if_it_never_read_it()
+    {
+        $subject = new CachingStream($this->given_a_token_stream([]));
+        $this->assertTrue($subject->isEOF());
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_when_seeking_past_the_end_of_the_cache()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                    Token::terminatingSymbol('2'),
+                    Token::terminatingSymbol('3'),
+                ]
+            )
+        );
+        $this->expectExceptionMessage('Can\'t seek ahead of the cache.');
+        $subject->seek(2);
+    }
+
+    public function it_throws_an_exception_when_advancing_past_the_end_of_the_stream()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                ]
+            )
+        );
+        $subject->advance();
+        $this->expectExceptionMessage('Trying to read past the end of the stream.');
+        $subject->advance();
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_advance_when_calling_nextToken()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                    Token::terminatingSymbol('2'),
+                    Token::terminatingSymbol('3'),
+                ]
+            )
+        );
+        $this->assertEquals(Token::terminatingSymbol('1'), $subject->nextToken());
+        $this->assertEquals(Token::terminatingSymbol('1'), $subject->nextToken());
+        $this->assertEquals(Token::terminatingSymbol('1'), $subject->nextToken());
+        $subject->advance();
+        $this->assertEquals(Token::terminatingSymbol('2'), $subject->nextToken());
+
+    }
+
+    /**
+     * @test
+     */
+    public function it_knows_when_it_is_at_the_end_of_the_stream_when_it_gets_there()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('blah'),
+                    Token::terminatingSymbol('blah'),
+                    Token::terminatingSymbol('blah'),
+                ]
+            )
+        );
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertTrue($subject->isEOF());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_advance()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                    Token::terminatingSymbol('2'),
+                    Token::terminatingSymbol('3'),
+                ]
+            )
+        );
+
+        $subject->advance();
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertEquals(Token::terminatingSymbol('3'), $subject->nextToken());
+        $this->assertTrue($subject->isEOF());
+    }
+
+    /**
+     * @test
+     */
+    public function it_knows_when_it_is_at_the_end_of_the_stream_after_rewinding()
+    {
+
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                    Token::terminatingSymbol('2'),
+                    Token::terminatingSymbol('3'),
+                ]
+            )
+        );
+
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertTrue($subject->isEOF());
+        $subject->seek(2);
+        $this->assertTrue($subject->isEOF());
+        $subject->seek(1);
+        $this->assertFalse($subject->isEOF());
+        $subject->seek(0);
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertFalse($subject->isEOF());
+        $subject->advance();
+        $this->assertTrue($subject->isEOF());
+    }
+
+    /**
+     * @test
+     */
+    public function it_knows_the_offset_it_is_at()
+    {
+        $subject = new CachingStream(
+            $this->given_a_token_stream(
+                [
+                    Token::terminatingSymbol('1'),
+                    Token::terminatingSymbol('2'),
+                    Token::terminatingSymbol('3'),
+                ]
+            )
+        );
+
+        $this->assertEquals(0, $subject->tell());
+        $subject->advance();
+        $subject->nextToken();
+        $subject->nextToken();
+        $subject->nextToken();
+        $this->assertEquals(1, $subject->tell());
+        $subject->advance();
+        $this->assertEquals(2, $subject->tell());
+        $subject->seek(1);
+        $this->assertEquals(1, $subject->tell());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_allow_rewinding_before_the_start()
+    {
+        $this->expectExceptionMessage("Offset can't be negative.");
+        $subject = new CachingStream($this->given_a_token_stream([Token::comment("")]));
+        $subject->seek(-1);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_if_passed_a_non_integer_number_to_seek()
+    {
+        $this->expectExceptionMessage("Offset must be an integer.");
+        $subject = new CachingStream($this->given_a_token_stream([Token::comment("")]));
+        $subject->seek(0.2);
+    }
+
+    /**
+     * @param Token[]
+     * @return TokenStreamInterface
+     */
+    private function given_a_token_stream(array $tokens)
+    {
+        return new TokenStream(
+            Mockery::mock(Lexer::class, ['getTokens' => new \ArrayIterator($tokens)])
+        );
+    }
+}
