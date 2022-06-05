4c87f26a976fc368f53149c3acf88975999d781d
Riccardo Bastianini <RBastianini@users.noreply.github.com>
2022-06-05 11:52:40 +0200

    Updated tested PHP version
    Also, misc typo corrections and removal of wordy extra array constant
    from token type class.

--- src/Language/AbstractSyntaxWalker.php
+++ src/Language/AbstractSyntaxWalker.php
@@ -19,66 +19,66 @@ use Polygen\Grammar\Unfoldable\NonTerminatingSymbol;
 interface AbstractSyntaxWalker
 {
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkDocument(Document $document, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkDefinition(Definition $definition, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkAssignment(Assignment $assignment, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkSequence(Sequence $sequence, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkProduction(Production $production, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkSubproduction(Subproduction $subproduction, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkSimpleAtom(SimpleAtom $atom, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkUnfoldableAtom(UnfoldableAtom $atom, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkNonTerminating(NonTerminatingSymbol $nonTerminatingSymbol, $context = null);
 
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkSubproductionUnfoldable(SubproductionUnfoldable $unfoldable, $context = null);
     /**
-     * @param mixed $context Any context that should be passed ito the wal function.
+     * @param mixed $context Any context that should be passed into the walk function.
      * @return mixed
      */
     public function walkAtomSequence(AtomSequence $atoms, $context = null);
